33f11b5c644d51cd7567e83b00a735936cb82656
Saifulin Nail <saifulin-nail@mail.ru>
2023-03-19 01:26:27 +0600

    change4

--- logistic/tests.py
+++ logistic/tests.py
@@ -6,4 +6,4 @@ class TestSampleView(TestCase):
     def test_view(self):
         client = APIClient()
         response = client.get('/api/v1/test/')
-        self.assertEqual(response.data, 'Hello world:)!')
+        self.assertEqual(response.data, 'Hello world!')
